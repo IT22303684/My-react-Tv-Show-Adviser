1c39552e974e670107be9e789e138fcd70c9890a
IT22303684 <it22303684@my.sliit.lk>
2024-11-18 16:06:32 +0530

    updated config

--- src/Api/tv-show.js
+++ src/Api/tv-show.js
@@ -1,23 +1,27 @@
 import axios from "axios";
-import {BASE_URL , API_KEY_PARAM , BACKDROP_BASE_URL} from "../config.js";
+import {BASE_URL ,   BACKDROP_BASE_URL} from "../config.js";
+
 
 export class TvShowAPI {
     static async fetchPopulars() {
-        const response = await axios.get(`${BASE_URL}tv/top_rated${API_KEY_PARAM}`);
+        const apiKey = import.meta.env.VITE_API_KEY;
+        const response = await axios.get(`${BASE_URL}tv/top_rated?api_key=${apiKey}`);
         console.log(response.data.results);
         return response.data.results;
 
     }
 
     static async fetchRecommendations(tvShowId) {
-        const response = await axios.get(`${BASE_URL}tv/${tvShowId}/recommendations${API_KEY_PARAM}`);
+        const apiKey = import.meta.env.VITE_API_KEY;
+        const response = await axios.get(`${BASE_URL}tv/${tvShowId}/recommendations?api_key=${apiKey}`);
         console.log(response.data.results);
         return response.data.results;
 
     }
 
     static async fetchByTitle(title) {
-        const response = await axios.get(`${BASE_URL}search/tv${API_KEY_PARAM}&query=${title}`);
+        const apiKey = import.meta.env.VITE_API_KEY;
+        const response = await axios.get(`${BASE_URL}search/tv?api_key=${apiKey}&query=${title}`);
         console.log(response.data.results);
         return response.data.results;
 
